7ecde5fc83aea6518b3511b4f2eeeef427b9afe3
howmehow <howmehow@gmail.com>
2020-06-16 18:34:41 +0100

    questions are being compared with correct answers and toast message is displayed accordingly

--- app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
@@ -36,12 +36,10 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP,0,0)
-            toast.show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
@@ -53,5 +51,17 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer){
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+//        currentIndex = (currentIndex + 1) % questionBank.size
+//        updateQuestion()
     }
 }
