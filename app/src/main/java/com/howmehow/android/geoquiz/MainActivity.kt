76e7acfd6b82f501886710e4383c477c9735e1e2
howmehow <howmehow@gmail.com>
2020-06-16 15:55:20 +0100

    Adding correct toast message on top of the screen. Gravity.top

--- app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.howmehow.android.geoquiz
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -18,8 +20,12 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
+
+
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.show()
         }
         falseButton.setOnClickListener { view: View ->
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
