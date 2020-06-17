05153d126711b081eec9bd4d7b111047a611bd91
howmehow <howmehow@gmail.com>
2020-06-17 13:42:42 +0100

    creating log messages to understand activity lifecycles

--- app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
@@ -3,12 +3,15 @@ package com.howmehow.android.geoquiz
 import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -60,6 +64,24 @@ class MainActivity : AppCompatActivity() {
 
         updateQuestion()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart is called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume is called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop is called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestory is called")
+    }
+
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
