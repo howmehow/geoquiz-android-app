37520e08e255a7ab5ba71a8c6d2e5fd9493e08a9
howmehow <howmehow@gmail.com>
2020-06-17 13:28:03 +0100

    creating two buttons to move from previous to next questions

--- app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,7 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -33,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -42,6 +45,15 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        previousButton.setOnClickListener {
+            if (currentIndex > 0){currentIndex = (currentIndex - 1) % questionBank.size}
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
@@ -61,7 +73,6 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
-//        currentIndex = (currentIndex + 1) % questionBank.size
-//        updateQuestion()
+
     }
 }
