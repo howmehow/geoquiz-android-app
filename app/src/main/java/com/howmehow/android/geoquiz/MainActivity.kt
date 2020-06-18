47fd06280f71db04f29174807eaae54ebd4d7101
howmehow <howmehow@gmail.com>
2020-06-18 13:37:15 +0100

    onSaveInstanceState is added

--- app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/geoquiz/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
 //        val provider: ViewModelProvider = ViewModelProviders.of(this)
 //        val quizViewModel = provider.get(QuizViewModel::class.java)
@@ -78,6 +82,12 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestory is called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
     override fun onPause() {
         super.onPause()
